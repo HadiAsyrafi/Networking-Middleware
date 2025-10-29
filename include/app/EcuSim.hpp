a4d5057f99f926cf7ceca1e75212712953031a58
Hadi Asyrafi <muhbinab@amd.com>
2025-10-29 11:16:31 -0400

    Add and use simulated ECU, with threading

---
+++ include/app/EcuSim.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <memory>
+
+#include <interface/MessageFactory.hpp>
+#include "middleware/MessageBus.hpp"
+#include "interface/Message.hpp"
+
+class ECU {
+protected:
+    MessageBus&       m_messageBus;
+    std::atomic<bool> m_running{false};
+    std::thread       m_thread;
+
+public:
+    ECU(MessageBus& bus) : m_messageBus(bus) {}
+
+    virtual ~ECU() {
+        stop();
+    }
+
+    void start() {
+        m_running = true;
+
+        // Note: &ECU::run is a pointer to the virtual "member function pointer"
+        // Note: "this" provides the object whose vtable to look at
+        m_thread  = std::thread(&ECU::run, this);
+
+        // Note: Functionally similar to:
+        // m_thread = std::thread([this]() { this->run(); });
+    }
+
+    void stop() {
+        m_running = false;
+        if (m_thread.joinable()) {
+            m_thread.join();
+        }
+    }
+
+protected:
+    virtual void run() = 0;
+    virtual MessagePtr createMessage() = 0;
+};
+
+// Concrete Implementation
+class RpmECU : public ECU {
+public:
+    RpmECU(MessageBus& bus) : ECU(bus) {}
+
+protected:
+    void run() override {
+        while (m_running) {
+            auto message = createMessage();
+            m_messageBus.publish(message);
+            std::this_thread::sleep_for(m_sleepTime);
+        }
+    }
+
+    MessagePtr createMessage() override {
+        auto rpmFactory = std::make_unique<RpmMsgFactory>();
+        return rpmFactory->factoryMethod();
+    }
+
+private:
+    std::chrono::milliseconds m_sleepTime{200 + (rand() % 501)};
+};
+
+// Concrete Implementation
+class TempECU : public ECU {
+public:
+    TempECU(MessageBus& bus) : ECU(bus) {}
+
+protected:
+    void run() override {
+        while (m_running) {
+            auto message = createMessage();
+            m_messageBus.publish(message);
+            std::this_thread::sleep_for(m_sleepTime);
+        }
+    }
+
+    MessagePtr createMessage() override {
+        auto tempFactory = std::make_unique<TempMsgFactory>();
+        return tempFactory->factoryMethod();
+    }
+
+private:
+    std::chrono::milliseconds m_sleepTime{500 + (rand() % 301)};
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,31 @@
 #include <iostream>
 #include <thread>
 
-#include <interface/MessageFactory.hpp>
 #include <middleware/MessageBus.hpp>
+#include <app/EcuSim.hpp>
 #include <app/EngineSubscriber.hpp>
 
 int main() {
     // Create Bus
     auto msgBus    = MessageBus();
 
-    // Create Subscriber
+    // Simulate Subscriber
     auto engineSub = makeEngineSubscriber();
     msgBus.subscribe("RPM", engineSub);
     msgBus.subscribe("TEMP", engineSub);
 
     // ECU Simulation
-    auto rpmFactory = std::make_unique<RpmMsgFactory>();
-    auto tempFactory = std::make_unique<TempMsgFactory>();
+    RpmECU rpmEcu(msgBus);
+    rpmEcu.start();
 
-    for (int i = 0; i < 5; ++i) {
-        // Create RPM message
-        auto rpmMsg = rpmFactory->factoryMethod();
-        msgBus.publish(rpmMsg);
+    TempECU tempEcu(msgBus);
+    tempEcu.start();
 
-        // Create Status message
-        auto statusMsg = tempFactory->factoryMethod();
-        msgBus.publish(statusMsg);
+    // Let it run for 3 seconds
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
+    rpmEcu.stop();
+    tempEcu.stop();
 
     return 0;
 }
